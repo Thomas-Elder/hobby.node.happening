188a48114186e044b653a1c6de98c236692974cf
Thomas Elder <thomas.charles.elder@gmail.com>
2017-02-13 20:14:25 +1100

    Created the server
    And implemented some basic tests, to test it responds to the relevent
    routes. The server is pretty much copied from the boxy number. Tried
    something lower level, but it was looking pretty complicated, and it's
    not really the goal of the project.

---
+++ server/server.js
@@ -0,0 +1,38 @@
+
+var http = require('http');
+var express = require('express');
+var app, server;
+
+var path = require('path');
+
+var Server = function(){
+  app = express();
+  server = http.createServer(app);
+
+  app.set('port', 8888);
+
+  app.set('views', path.join(__dirname, './views'));
+  app.set('view engine', 'pug');
+
+  app.get('/', function(req, res){
+    res.render('index', {title: "H A P P E N I N G"})
+  });
+
+  app.get('*', function(req, res){
+    res.render('none', {title: "N O T H I N G T O S E E H E R E"})
+  });
+
+  app.use(express.static(path.join(__dirname, './client')));
+};
+
+Server.prototype.start = function() {
+  server.listen(app.get('port'), function(){
+    console.log('Express server listening on port: ' + app.get('port'));
+  })
+};
+
+Server.prototype.stop = function() {
+  server.close();
+};
+
+module.exports = Server;
---
+++ spec/server/server_spec.js
@@ -0,0 +1,57 @@
+var Server = require('../../server/server');
+
+var request = require('request');
+
+describe('Server', function(){
+    
+  var server;
+  
+  var port = 8888;
+  var url = 'http://localhost:' + port;
+
+  beforeEach(function(done){
+    server = new Server();
+    console.log('Starting the server...');
+    server.start();
+    done();
+  });
+  
+  afterEach(function(done){
+    server.stop();
+    done();
+  });
+    
+  describe('connection tests', function(){
+      
+    it('should return OK statusCode to a request for "/"', function(done){
+        
+      request.get(
+      {
+        'url':url + "/"
+      },
+      function(err, res){
+
+        if(res === undefined)
+          throw new Error(err);
+        
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it('should return OK statusCode to a request for "/gibberish"', function(done){
+      request.get(
+        {
+          'url':url + "/gibberish"
+        },
+        function(err, res){
+
+          if(res === undefined)
+            throw new Error(err);
+          
+          expect(res.statusCode).toBe(200);
+          done();
+      });
+    });   
+  });  
+});
