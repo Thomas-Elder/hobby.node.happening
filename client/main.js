fa0a7a2dc6848c783c121c859b1c5a519b6273cc
Thomas Elder <thomas.charles.elder@gmail.com>
2017-02-22 09:31:38 +1100

    Created login and chat sections in the page
    Login is shown first, and currently just asks for a name. Once login is
    clicked the chat section is shown.
    
    Next step new events to be fired on login, including the users name to
    let the other users who's typing.

--- client/main.js
+++ client/main.js
@@ -1,27 +1,31 @@
 window.onload = function(){
   var socket = io();
+  
+  var name = "";
 
+  $('#login-section').show();
+  $('#chat-section').hide();
+  
+  $('#login').click(function(){
+    name = $('#name').val();
+
+    $('#login-section').hide();
+    $('#chat-section').show();
+  });
+  
+  $('#send').click(function(){
+    var msg = $('#message').val();
+    socket.emit('new-message', msg);
+  });
   socket.on('user-connected', function(){
-
-    console.log('new user joined');
     $('#chat').append('<li>A new user has joined</li>');
   });
 
   socket.on('user-disconnected', function(){
-
-    console.log('user left');
     $('#chat').append('<li>A user has left</li>');
   });
 
   socket.on('new-message', function(msg){
-    console.log(msg);
     $('#chat').append('<li>' + msg + '</li>');
-  });
-
-  $('#send').click(function(){
-
-    var msg = $('#message').val();
-    console.log(msg);
-    socket.emit('new-message', msg);
   });
 };
