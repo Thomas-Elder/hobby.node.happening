83c1fa305dc1564be30d26659036cea4c1cc8119
Thomas Elder <thomas.charles.elder@gmail.com>
2017-02-22 15:16:51 +1100

    Updated client side code to handle data object
    So the clients now display name of user who logged in and sent the
    message etc.

--- client/main.js
+++ client/main.js
@@ -7,18 +7,24 @@ window.onload = function(){
   $('#chat-section').hide();
   
   $('#login').click(function(){
+    var data = {};
     name = $('#name').val();
+    data.name = name;
 
     $('#login-section').hide();
     $('#chat-section').show();
 
-    socket.emit('login', name);
+    socket.emit('login', data);
   });
   
   $('#send').click(function(){
-    var msg = $('#message').val();
-    socket.emit('new-message', msg);
+    var data = {};
+    data.name = name;
+    data.msg = $('#message').val();
+
+    socket.emit('new-message', data);
   });
+  
   socket.on('user-connected', function(){
     $('#chat').append('<li>A new user has joined</li>');
   });
@@ -27,11 +33,11 @@ window.onload = function(){
     $('#chat').append('<li>A user has left</li>');
   });
 
-  socket.on('new-message', function(msg){
-    $('#chat').append('<li>' + msg + '</li>');
+  socket.on('new-login', function(data){
+    $('#chat').append('<li>User: ' + data.name + ' has joined the chat.</li>');
   });
 
-  socket.on('new-login', function(user){
-    $('#chat').append('<li>User: ' + user + ' has joined the chat.</li>');
+  socket.on('new-message', function(data){
+    $('#chat').append('<li>' + data.name + ":" + data.msg + '</li>');
   });
 };
