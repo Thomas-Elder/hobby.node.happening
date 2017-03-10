13a376a8399de2be6c5d085976a41b740826ff76
Thomas Elder <thomas.charles.elder@gmail.com>
2017-03-10 20:56:08 +1100

    Tidy unnecessary connection/disconnection in spec

--- spec/server/events/connection_spec.js
+++ spec/server/events/connection_spec.js
@@ -18,97 +18,99 @@ describe('connection', function(){
   server = new Server();
   server.start();
 
-  describe('connection', function(){
+  beforeEach(function(done){
 
-    it('should emit "connect" event back to this client on connection', function(done){           
-      
-      client_a = io_client(url, socketOptions);
+    // Connect a client socket to the server
+    client_a = io_client(url, socketOptions);
+    client_b = io_client(url, socketOptions);
 
-      client_a.on('connect', function(){
+    // Log connection
+    client_a.on('connect', function(){
         
-        client_a.on('disconnect', function(){
-
-          client_a.on('connect', function(){
-            expect(true).toEqual(true);
-
-            client_a.disconnect();
-            done();
-          });
-
-          client_a.connect(url, socketOptions);
-        });
-
-        client_a.disconnect();
+        client_b.on('connect', function(){
+          done();
       });
     });
 
-    it('should emit "disconnect" event back to this client on disconnection', function(done){           
+    // Log connection error
+    client_a.on('connect_error', function(err){
+      console.log('client_a not connected, there was an error.', err);
+      done();
+    });
 
-      client_a = io_client(url, socketOptions);
+    client_b.on('connect_error', function(err){
+      console.log('client_b not connected, there was an error.', err);
+      done();
+    });
+  });
+  
+  afterEach(function(done){
+    client_a.disconnect(true);
+    client_b.disconnect(true);
+    done();
+  });
 
-      client_a.on('connect', function(){
-        
-        client_a.on('disconnect', function(){
+  describe('connection', function(){
+
+    it('should emit "connect" event back to this client on connection', function(done){           
+
+      client_a.on('disconnect', function(){
+
+        client_a.on('connect', function(){
           expect(true).toEqual(true);
 
           client_a.disconnect();
           done();
         });
 
-        client_a.disconnect();
+        client_a.connect(url, socketOptions);
       });
+
+      client_a.disconnect();
+
+    });
+
+    it('should emit "disconnect" event back to this client on disconnection', function(done){           
+
+      client_a.on('disconnect', function(){
+        expect(true).toEqual(true);
+
+        client_a.disconnect();
+        done();
+      });
+
+      client_a.disconnect();
+
     });
 
     it('should emit "user-connected" event to other clients on connection', function(done){    
-      
-      client_a = io_client(url, socketOptions);
-      client_b = io_client(url, socketOptions);
 
-      client_a.on('connect', function(){
+      client_a.on('disconnect', function(){
 
-        client_b.on('connect', function(){
-
-          client_a.on('disconnect', function(){
-
-            client_b.on('user-connected', function(){
-              expect(true).toEqual(true);
-
-              client_a.disconnect();
-              client_b.disconnect();
-              done();
-            });
-
-            client_a.connect(url, socketOptions);
-          });
-
-          client_a.disconnect();
+        client_b.on('user-connected', function(){
+          expect(true).toEqual(true);
+          done();
         });
+
+        client_a.connect(url, socketOptions);
       });
+
+      client_a.disconnect();
+
     });
 
     it('should emit "user-disconnected" event to other clients on disconnection', function(done){           
-     
-      client_a = io_client(url, socketOptions);
-      client_b = io_client(url, socketOptions);
 
-      client_a.on('connect', function(){
-        
-        client_b.on('connect', function(){
+      client_a.on('disconnect', function(){
 
-          client_a.on('disconnect', function(){
-
-            client_b.on('user-disconnected', function(){
-              expect(true).toEqual(true);
-
-              client_a.disconnect();
-              client_b.disconnect();
-              done();
-            });
-          });
-
-          client_a.disconnect();
+        client_b.on('user-disconnected', function(){
+          expect(true).toEqual(true);
+          done();
         });
       });
+
+      client_a.disconnect();
+
     });
   });
 });
